a2712302fc1827a323243d0d84ee43c962e332d1
German Cruz <cruzgerman216@yahoo.com>
2020-03-17 20:36:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,7 +11,11 @@ class Artist
   def self.all
     @@all
   end
-
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
   def new_song(name, genre)
     Song.new(name, self, genre)
   end
